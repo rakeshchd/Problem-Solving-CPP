d059dc184480b0f5129dbd4bfc5b52bf69a0c6af
Rakesh Choudhary <120676797+rakeshchd@users.noreply.github.com>
2022-12-24 10:14:07 +0530

    Time: 17 ms (63.65%), Space: 11 MB (29.99%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -6,8 +6,21 @@ public:
         if(m*n > r*c || m*n < r*c){
             return mat;
         }
+        vector<int> fun;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                fun.push_back(mat[i][j]);
+            }
+        }
         vector<vector<int>> new_matrix(r, vector<int>(c, 0));
-        for(int i=0; i<r*c; i++) new_matrix[i/c][i%c] = mat[i/n][i%n];
+        int k = 0;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                new_matrix[i][j] = fun[k];
+                k++;
+            }
+        }
+        // for(int i=0; i<r*c; i++) new_matrix[i/c][i%c] = mat[i/n][i%n];
         return new_matrix;
     }
 };
