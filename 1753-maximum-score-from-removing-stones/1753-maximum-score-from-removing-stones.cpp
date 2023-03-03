f6a46126964b38a5f8a02ff61a9fc261f2eefe5b
Rakesh Choudhary <120676797+rakeshchd@users.noreply.github.com>
2023-03-04 03:35:02 +0530

    Time: 90 ms (64.62%), Space: 6 MB (38.38%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        int score = 0;
+        vector<int> v = {a, b, c};
+        sort(v.begin(), v.end(), greater<int>());
+        while(v[0]>0 && v[1]>0){
+            v[0] -= 1;
+            v[1] -= 1;
+            score++;
+            sort(v.begin(), v.end(), greater<int>());
+        }
+        while(v[1]>0 && v[2]>0){
+            v[1] -= 1;
+            v[2] -= 1;
+            score++;
+            sort(v.begin(), v.end(), greater<int>());
+        }
+        return score;
+    }
+};
